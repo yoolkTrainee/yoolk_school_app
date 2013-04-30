58c6f6abdbf3dbf4a4ba30b69a522302bf610104
Bunhok <cbunhok@gmail.com>
2013-04-30 09:34:16 +0700

    new_project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoolkSchoolApp::Application.config.secret_token = '4a7ddd16239c489bc275c21802f52a4fe2b6f977b2f6b775e1c420d66116e679f8aac9cd2089c345f6d1db8a29dc825c86874a8ed9659cd65216936804360ed6'
